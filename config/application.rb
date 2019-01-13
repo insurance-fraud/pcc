6377f192c63713b026667847ea54d5b92e3b3b7e
Nikola Đuza <nikolaseap@gmail.com>
2019-01-13 13:14:03 +0100

    Log to standard output

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module Pcc
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Log to standard out
+    config.logger = Logger.new(STDOUT)
   end
 end
